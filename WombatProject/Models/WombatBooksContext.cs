e93b511b95a1c283487990c2ab52fd34f4e18cd0
Veronika Takács <roni9764@gmail.com>
2021-01-11 13:01:52 +0100

    Add DbSets to include new models

--- WombatProject/Models/WombatBooksContext.cs
+++ WombatProject/Models/WombatBooksContext.cs
@@ -8,9 +8,11 @@ namespace WombatLibrarianApi.Models
         {
         }
 
-        public DbSet<Book> BookShelfItems { get; set; }
-        public DbSet<Book> WishListItems { get; set; }
+        public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Wishlist> WishlistItems { get; set; }
+        public DbSet<Bookshelf> BookshelfItems { get; set; }
     }
 }
