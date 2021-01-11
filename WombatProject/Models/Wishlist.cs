caac7f9ae227d84ff07bbb6c3384ceb6d0ac564e
Veronika Takács <roni9764@gmail.com>
2021-01-11 13:01:18 +0100

    Add Wishlist model

---
+++ WombatProject/Models/Wishlist.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WombatLibrarianApi.Models
+{
+    [Keyless]
+    public class Wishlist
+    {
+        public int UserId { get; set; }
+        public string BookId { get; set; }
+
+        public User User { get; set; }
+        public Book Book { get; set; }
+    }
+}
