0b925ffad6dfabd601267b63730d42b47c36f567
Veronika Takács <roni9764@gmail.com>
2021-01-21 14:41:13 +0100

    Add migration and update database with Identity framework related default tables

--- WombatProject/Migrations/20210121131134_AddIdentity.Designer.cs
+++ WombatProject/Migrations/20210121131134_AddIdentity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WombatLibrarianApi.Models;
 
 namespace WombatLibrarianApi.Migrations
 {
     [DbContext(typeof(WombatBooksContext))]
-    partial class WombatBooksContextModelSnapshot : ModelSnapshot
+    [Migration("20210121131134_AddIdentity")]
+    partial class AddIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
